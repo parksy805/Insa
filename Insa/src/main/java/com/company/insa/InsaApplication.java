c8ef82fdafd1c93512b6c669fbc3b97122a68b30
pino2 <pino2@DESKTOP-4MMD6P3>
2022-02-18 16:46:04 +0900

    first commit

---
+++ Insa/src/main/java/com/company/insa/InsaApplication.java
@@ -0,0 +1,13 @@
+package com.company.insa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsaApplication.class, args);
+	}
+
+}
---
+++ Insa/src/test/java/com/company/insa/InsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.insa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
